43c38d642575dcb85123e5f890b78a042849e17e
Kishore B <kbasuvappan@gmail.com>
2026-09-23 05:31:08 +0000

    map example

---
+++ Day-05/terraform.tfvars
@@ -0,0 +1,6 @@
+resource_group_name = {
+    dev = "DEV-RG"
+    QA = "QA-RG"
+    PROD = "PROD-RG"
+}
+location = "west europe"
---
+++ Day-05/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+    description = "Name of the RG"
+    type = map(string)
+}
+
+variable "location" {
+    description = "azure location"
+    type = string
+}
