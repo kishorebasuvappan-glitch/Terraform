77ad279f95a8723f13474678c7861536786ec1f1
Kishore B <kbasuvappan@gmail.com>
2026-09-23 05:43:34 +0000

    map object example

--- Day-06/main.tf
+++ Day-06/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg"{
     for_each = var.resource_group_name
-    name = each.value
-    location = var.location
+    name = each.value.resource_group_name
+    location = each.value.location
 }
